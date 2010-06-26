eaf89471ec9603c0b1925ed67860c2c85a091d11
chaitanya <gopi.daiict@gmail.com>
2010-06-26 21:02:58 +0530

    intermediate commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import java.util.*;
+import java.util.regex.*;
 import play.mvc.*;
 
 import models.*;
@@ -16,9 +18,20 @@ public class Application extends Controller {
 
 	public static void addStyle(String field, String value) {
 		if (validation.hasErrors()) {
-			render("Application/index.html");
+			render("Application/index.html", Pattern.CASE_INSENSITIVE);
 		}
-		Style style = new Style(field, value).save();
+		
+//		Style style = new Style(field, value).save();
+		Style style = Style.find("byField", field).first();
+		String css = style.value;
+		
+		Pattern pattern = Pattern.compile(".contact(\\{(([a-z]-?)*:(^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})|[0-9]*([a-z]-?)*;)*?)\\})");
+		Matcher matcher = pattern.matcher(css);
+		
+		while(matcher.find()) {
+			System.out.println("matched" + matcher.start() + ", " + matcher.end() + ", " + matcher.group());
+		}
+		
 		renderJSON(style);
 	}
 }
