bd67de76f86fc2f70ea29d7b8368f3a5ebd59788
chaitanya <gopi.daiict@gmail.com>
2010-06-26 17:35:55 +0530

    added minimal-support for custom css; fetching css from db.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -9,16 +9,16 @@ public class Application extends Controller {
 	public static void index() {
 		String name = request.domain.replace(".tst.it", "");
 		User user = User.find("byName", name).first();
-		render(user);
+		Style style = Style.find("byField", name).first();
+		
+		render(user, style);
 	}
 
 	public static void addStyle(String field, String value) {
 		if (validation.hasErrors()) {
 			render("Application/index.html");
 		}
-		Style style = new Style(field, value);
-		style.save();
-		index();
+		Style style = new Style(field, value).save();
+		renderJSON(style);
 	}
-
 }
