fdafabeb85c88631353283edc93e2a527d4c1c5e
chaitanya <gopi.daiict@gmail.com>
2010-08-10 02:53:17 +0530

    1, moved the js to main.js 2, cleaned up the welcome page. 3, added some new styles.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,3 +9,25 @@ $(function(){
         return false;
     });
 });
+//FB.ui({
+//    method: 'stream.publish',
+//    message: 'geeez! shrrrts is cool :P',
+//    attachment: {
+//        name: 'shrrts.com',
+//        caption: 'The New Shrrrts',
+//        description: ('checkout the coolest new shrrrts place.'),
+//        href: 'http://shrrts.com'
+//    },
+//    action_links: [{
+//        text: 'shrrts.com',
+//        href: 'http://shrrts.com'
+//    }],
+//    user_message_prompt: 'Share your thoughts about Connect'
+//}, function(response){
+//    if (response && response.post_id) {
+//        alert('Post was published.');
+//    }
+//    else {
+//        alert('Post was not published.');
+//    }
+//});
